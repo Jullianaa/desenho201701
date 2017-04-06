753de5f8015a247ea947119725e3bdf87fa1f5c6
Lucas Rufino <lucaslrt@outlook.com>
2017-04-06 11:24:41 -0300

    Creating SchoolClass Model Signed-off-by: Stefania Bezerra da Silva  <stefaniabezerra95@gmail.com> Signed-off-by: Thiagotrfm <t_moreira@outlook.com>
    Signed-off-by: Lucas Rufino <lucaslrt@outlook.com>

---
+++ Schoolep/app/src/main/java/com/schoolep/desenho/schoolep/models/SchoolClass.java
@@ -0,0 +1,21 @@
+package com.schoolep.desenho.schoolep.models;
+
+import java.util.Date;
+
+
+public class SchoolClass extends Event {
+
+    private int absentClass;
+
+    public SchoolClass(Date dateEvent, Date startTime, Date endTime, String localEvent, String discipline) {
+        super(dateEvent, startTime, endTime, localEvent, discipline);
+    }
+
+    public int getAbsentClass() {
+        return absentClass;
+    }
+
+    public void setAbsentClass(int absentClass) {
+        this.absentClass = absentClass;
+    }
+}
