aa0d7a5dbd4385a2596801b27999b2109b330687
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-04-06 12:16:20 -0300

    Creating Exam model
    Signed-off-by: Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
    Signed-off-by: Thiagotrfm  <t_moreira@outlook.com>
    Signed-off-by: Lucas Rufino <lucaslrt@outlook.com>

---
+++ Schoolep/app/src/main/java/com/schoolep/desenho/schoolep/models/Exam.java
@@ -0,0 +1,29 @@
+package com.schoolep.desenho.schoolep.models;
+
+import java.util.List;
+
+public class Exam {
+    private List <String> contentExam;
+    private float grade;
+
+    public Exam(float grade) {
+        this.grade = grade;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public void addContentExam (String content){
+        this.contentExam.add(content);
+    }
+
+    public List<String> getContentExam (){
+        return this.contentExam;
+    }
+
+}
