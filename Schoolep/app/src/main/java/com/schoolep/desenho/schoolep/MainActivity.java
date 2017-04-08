9697777f681d767de1ebb8414a236539d32a2367
Jullianaa <julliana.coutoalmeida@gmail.com>
2017-04-08 12:39:54 -0300

    Adding xml of list of discipline class and mensage of confirmation

--- Schoolep/app/src/main/java/com/schoolep/desenho/schoolep/MainActivity.java
+++ Schoolep/app/src/main/java/com/schoolep/desenho/schoolep/MainActivity.java
@@ -1,5 +1,7 @@
 package com.schoolep.desenho.schoolep;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(
+                R.layout.activity_main);
+    }
+
+    public void dialog(){
+        new AlertDialog.Builder(this)
+                .setTitle("Confirmação de Turma")
+                .setMessage("Hey, você participa mesmo dessa turma?")
+                .setNegativeButton("Não", null)
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //chamar tela listSubject.xml
+                    }
+                }).create().show();
     }
 }
