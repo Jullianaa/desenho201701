d1083ef4562ceb765079d5a027c3b6430351e0b5
inddiara <indiara96d@gmail.com>
2017-05-05 15:57:59 -0300

    Exam activity moved to exam directory

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/exam/ExamActivity.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/exam/ExamActivity.java
@@ -1,4 +1,4 @@
-package com.schoolapp.desenho.schoolapp;
+package com.schoolapp.desenho.schoolapp.exam;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.schoolapp.desenho.schoolapp.R;
 
 
 public class ExamActivity extends AppCompatActivity {
