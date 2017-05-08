874cae3ff447cb3a02a7187006aa9a29dccb4a22
inddiara <indiara96d@gmail.com>
2017-05-08 19:19:45 -0300

    Getting all disciplines
    Signed-off-by: inddiara <indiara96d@gmail.com>
    Signed-off-by: Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/presenter/ExamPresenter.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/presenter/ExamPresenter.java
@@ -1,9 +1,26 @@
 package com.schoolapp.desenho.schoolapp.presenter;
 
+import android.content.Context;
+
+import com.schoolapp.desenho.schoolapp.dao.ExamDAO;
+import com.schoolapp.desenho.schoolapp.models.Exam;
+
+import java.util.List;
+
 /**
  * Created by indiara on 08/05/17.
  */
 
 public class ExamPresenter {
+    private Context context;
 
+    public ExamPresenter(Context context){
+        this.context = context;
+    }
+
+    public List<Exam> getAllExams(Integer disciplineId){
+        ExamDAO examDAO = new ExamDAO(context);
+        List<Exam> allExams = examDAO.getAllExams(disciplineId);
+        return allExams;
+    }
 }
--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/view/ListExamsFragment.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/view/ListExamsFragment.java
@@ -5,9 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.schoolapp.desenho.schoolapp.R;
+import com.schoolapp.desenho.schoolapp.models.Exam;
+import com.schoolapp.desenho.schoolapp.presenter.ExamPresenter;
+
+import java.util.List;
 
 
 public class ListExamsFragment extends Fragment {
@@ -16,7 +21,6 @@ public class ListExamsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -24,8 +28,11 @@ public class ListExamsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_list_exams, container, false);
 
         ListView examList = (ListView) view.findViewById(R.id.lista);
-
-
+        ExamPresenter examPresenter = new ExamPresenter(getContext());
+        List<Exam> allDisciplines = examPresenter.getAllExams(1);
+        ArrayAdapter<Exam> adapter = new ArrayAdapter<Exam>(this.getContext(),
+                android.R.layout.simple_list_item_1, allDisciplines);
+        examList.setAdapter(adapter);
         return view;
     }
 
