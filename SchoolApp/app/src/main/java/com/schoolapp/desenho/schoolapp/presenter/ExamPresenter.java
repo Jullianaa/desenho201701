90ac3c17cda1a5330239f041f5464c229f23497a
Thiagotrfm <t_moreira@outlook.com>
2017-05-08 22:09:11 -0300

    Creating discipline spinner selector and DisciplinePresenter

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/presenter/ExamPresenter.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/presenter/ExamPresenter.java
@@ -1,15 +1,9 @@
 package com.schoolapp.desenho.schoolapp.presenter;
 
 import android.content.Context;
-
 import com.schoolapp.desenho.schoolapp.dao.ExamDAO;
 import com.schoolapp.desenho.schoolapp.models.Exam;
-
 import java.util.List;
-
-/**
- * Created by indiara on 08/05/17.
- */
 
 public class ExamPresenter {
     private Context context;
