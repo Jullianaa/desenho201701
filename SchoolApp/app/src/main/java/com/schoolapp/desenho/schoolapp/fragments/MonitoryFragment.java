a51e307f60335b60468b41f7db3f0534cda09b54
Jullianaa <julliana.coutoalmeida@gmail.com>
2017-05-10 15:49:07 -0300

    Adding list to monitory

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/MonitoryFragment.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/MonitoryFragment.java
@@ -2,11 +2,20 @@ package com.schoolapp.desenho.schoolapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.schoolapp.desenho.schoolapp.R;
+import com.schoolapp.desenho.schoolapp.dao.MonitoryDAO;
+import com.schoolapp.desenho.schoolapp.models.Monitory;
+
+import java.util.List;
+
+import static com.schoolapp.desenho.schoolapp.dao.MonitoryDAO.*;
 
 public class MonitoryFragment extends Fragment {
 
@@ -18,7 +27,16 @@ public class MonitoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        List<Monitory> monitories = MonitoryDAO.getMonitory() ;
+
+        ListView Monitory = (ListView) findViewById(R.id.lista);
+        ArrayAdapter<Monitory> adapter = new ArrayAdapter<Monitory>(this,
+                android.R.layout.fragment_monitory, Monitory);
+
+        listMonitory.setAdapter(adapter);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_monitory, container, false);
     }
 }
+
+
