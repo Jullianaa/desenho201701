53814ef2b7ecc5832589185b23df9924a8918fa6
Gabriel Viana <gabrielv14@gmail.com>
2017-05-10 17:04:41 -0300

    Student Disciplines adjusts in Discipline Fragment

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/DisciplineFragment.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/DisciplineFragment.java
@@ -1,27 +1,42 @@
 package com.schoolapp.desenho.schoolapp.fragments;
-
+import java.util.ArrayList;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.schoolapp.desenho.schoolapp.R;
 import com.schoolapp.desenho.schoolapp.databaseHelper.UserDataHelper;
 import com.schoolapp.desenho.schoolapp.models.Student;
+import com.schoolapp.desenho.schoolapp.dao.DisciplineDAO;
+import com.schoolapp.desenho.schoolapp.models.DisciplineClass;
+import com.schoolapp.desenho.schoolapp.models.Student;
 
-public class DisciplineFragment extends Fragment {
+
+public class DisciplineFragment extends ListFragment {
+
     private Student userData;
+
+    ArrayList<String> disciplinesName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        this.getDisciplines();
         //  Set up the initial state for acessing the DB
         Activity actualActivity = getActivity();
         UserDataHelper userDH = new UserDataHelper(actualActivity);
-
+        userDH.setStudent();
         // Gets the user data
         this.setUserData(userDH.getUserInstance());
     }
@@ -37,5 +52,17 @@ public class DisciplineFragment extends Fragment {
     // Private setters
     private void setUserData(Student userData) {
         this.userData = userData;
+    }
+
+    public void getDisciplines() {
+        if (this.userData!=null) {
+          ArrayList<DisciplineClass> studentDisciplines = this.userData.getStudentDisciplinesClasses();
+          ArrayList<String>  disciplines = new ArrayList<>();
+          for( DisciplineClass discipline : studentDisciplines) {
+              disciplines.add(discipline.getClassName());
+          }
+        }else{
+          //nothing to do
+        }
     }
 }
