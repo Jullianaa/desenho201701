d2c4443ce87c1aa17d4bc049ae75cf6fc55dc64e
Gabriel Viana <gabrielv14@gmail.com>
2017-05-10 22:44:45 -0300

    Fixing DisciplineFragment conflict when directory changed

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/discipline/DisciplineFragment.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/discipline/DisciplineFragment.java
@@ -1,33 +1,37 @@
-package com.schoolapp.desenho.schoolapp.fragments;
+package com.schoolapp.desenho.schoolapp.fragments.discipline;
+
 import java.util.ArrayList;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.schoolapp.desenho.schoolapp.R;
-import com.schoolapp.desenho.schoolapp.databaseHelper.UserDataHelper;
-import com.schoolapp.desenho.schoolapp.models.Student;
 import com.schoolapp.desenho.schoolapp.dao.DisciplineDAO;
+import com.schoolapp.desenho.schoolapp.databaseHelper.UserDataHelper;
 import com.schoolapp.desenho.schoolapp.models.Discipline;
 import com.schoolapp.desenho.schoolapp.models.Student;
+import com.schoolapp.desenho.schoolapp.models.DisciplineClass;
 
 
 public class DisciplineFragment extends ListFragment {
 
     private Student userData;
+    private ArrayAdapter adapter;
+    private ArrayList<String> disciplinesName;
+    private ArrayList<DisciplineClass> studentDisciplineClasses;
 
-    ArrayList<String> disciplinesName;
+    // Communication between PhotoFragment and EventMapFragment
+    private InterfaceCommunication mListener;
+
+    public interface InterfaceCommunication {
+        void getDisciplineClassId(int disciplineClassId);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,26 @@ public class DisciplineFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discipline, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_discipline, container, false);
+
+        adapter = new ArrayAdapter(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                disciplinesName
+        );
+        setListAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+        mListener.getDisciplineClassId(studentDisciplineClasses.get(position).getDisciplineClassId());
+
+
     }
 
 
