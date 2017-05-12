97682ce8451af99797ebd1331228c5a1177b1766
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2017-05-12 00:12:18 -0300

    Enhanced list fragment; Alternative layouts check for list fragment; Minor fixes.

--- SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/presence/PresenceListFragment.java
+++ SchoolApp/app/src/main/java/com/schoolapp/desenho/schoolapp/fragments/presence/PresenceListFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.schoolapp.desenho.schoolapp.R;
@@ -37,19 +38,25 @@ public class PresenceListFragment extends Fragment implements AdapterView.OnItem
         // Use fakeData for debug process
         ArrayList<StudentPresence> fakeData = new ArrayList<>();
         fakeData.add(new StudentPresence("Disciplina 1", 0));
-        fakeData.add(new StudentPresence("Disciplina 2", 3));
-        fakeData.add(new StudentPresence("Disciplina 3", 5));
+        fakeData.add(new StudentPresence("Disciplina 2", 0));
+        fakeData.add(new StudentPresence("Disciplina 3", 0));
+
+        // Clean the discipline data
+        final ArrayList<StudentPresence> cleanedList = this.getClassesWithMiss(fakeData);
 
         // Get the ListView
         ListView list = (ListView)getActivity().findViewById(R.id.presence_list);
 
         // Create and set the custom adapter for the listview
         final StudentPresenceAdapter adapter =
-                new StudentPresenceAdapter(getActivity(), fakeData);
+                new StudentPresenceAdapter(getActivity(), cleanedList);
         list.setAdapter(adapter);
 
         // Set a listener for each item
         list.setOnItemClickListener(this);
+
+        // Set Layout View according to the data
+        this.setLayoutByMisses(cleanedList);
     }
 
     @Override
@@ -57,4 +64,55 @@ public class PresenceListFragment extends Fragment implements AdapterView.OnItem
         Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 
+    /* This function gets an StudentPresence ArrayList and return a new ArrayList with only disciplines
+     * that has misses on it.
+     *
+     * @param ArrayList<StudentPresence> presenceList
+     * @return ArrayList<StudentPresence> newList
+     */
+    private ArrayList<StudentPresence> getClassesWithMiss(ArrayList<StudentPresence> presenceList){
+        assert presenceList != null : "NULLPOINTER found! Check getClassesWithMiss...";
+
+        // Creates a empty presence arraylist
+        ArrayList<StudentPresence> newList = new ArrayList<>();
+
+        // Interate over the presenceList
+        for(StudentPresence presenceData : presenceList){
+            // If the discipline has more than 0 misses, add to the new array
+            if(presenceData.getClassesMissed() > 0){
+                newList.add(presenceData);
+            } else{
+                // DO NOTHING
+            }
+        }
+
+        return newList;
+    }
+
+
+    /* This function defines the visibility for the ListView and TextView on the fragment, depending
+     * on the available data.
+     *
+     * @param ArrayList<StudentPresence> presenceList
+     */
+    private void setLayoutByMisses (ArrayList<StudentPresence> presenceList){
+        assert presenceList != null : "NULLPOINTER found! Check setLayoutByMisses...";
+
+        // Getting the list size to define the layout
+        final Integer listSize = presenceList.size();
+        assert listSize >= 0 : "Invalid ArrayList size! Check setLayoutByMisses...";
+
+        // Get the layout items to be set
+        ListView list = (ListView)getActivity().findViewById(R.id.presence_list);
+        TextView infoText = (TextView)getActivity().findViewById(R.id.presence_text_info);
+
+        // Disable the info text if the data size is greater than 0
+        if(listSize > 0){
+            infoText.setVisibility(View.INVISIBLE);
+            list.setVisibility(View.VISIBLE);
+        } else {
+            infoText.setVisibility(View.VISIBLE);
+            list.setVisibility(View.INVISIBLE);
+        }
+    }
 }
